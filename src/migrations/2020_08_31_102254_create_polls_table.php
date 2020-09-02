7f36c624c524453a69230fa78db62bd2413a678a
sergio bogatskyy <sergiobogatsky@gmail.com>
2020-09-02 10:27:14 +0200

    trying...

--- src/migrations/2020_08_31_102254_create_polls_table.php
+++ src/migrations/2020_08_31_102254_create_polls_table.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace SergioBogatsky\TelegramPollsWithoutGroup\migrations;
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
